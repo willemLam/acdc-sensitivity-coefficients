9316b0bbe14ad02ff69747429273a7f9b86b19c3
willem <willem.lambrichts@gmail.com>
2023-05-23 17:10:07 +0200

    clean master branch

--- Helper functions/Get_multiphase_Node_indices.m
+++ Helper functions/Get_multiphase_Node_indices.m
@@ -12,13 +12,6 @@ function idx = Get_multiphase_Node_indices(idx1,Grid_para)
 
     idx.vscac_pq = polyphase_indices(idx1.vscac_pq,n_ph);
     idx.vscac_vq = polyphase_indices(idx1.vscac_vq,n_ph);
-%     
-%     idx.pqac_p = polyphase_indices(idx1.pqac_p,n_ph);
-%     idx.pqac_q = polyphase_indices(idx1.pqac_q,n_ph);
-%     idx.vscac_pq_p = polyphase_indices(idx1.vscac_pq_p,n_ph);
-%     idx.vscac_pq_q = polyphase_indices(idx1.vscac_pq_q,n_ph);
-    idx.vscac_vq_v = polyphase_indices(idx1.vscac_vq_v,n_ph);
-    idx.vscac_vq_q = polyphase_indices(idx1.vscac_vq_q,n_ph);
 
     idx.vscdc_pq = (n_ph-1)*n_ac + idx1.vscdc_pq;
     idx.vscdc_vq = (n_ph-1)*n_ac + idx1.vscdc_vq;
--- include_losses_filter_in_E.m
+++
@@ -1,7 +0,0 @@
-function E_star = include_losses_filter_in_E(E_star,YY)
-
-
-
-
-
-end
